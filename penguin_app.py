76e3f16cbea7c2bf3a7e51fb030ccf96b1fe2490
ranaz <phrezh@gmail.com>
2021-08-20 12:37:47 +0100

    Add initial file

---
+++ penguin_app.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 13 13:40:34 2021
+
+@author: razaknart
+
+Simple Web Application for classifying Penguins Species
+"""
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+import joblib
+
+
+st.title('Penguin Prediction App')
+
+st.write("""
+This app predicts the **Palmer Penguin** species!
+Data obtained from the [palmerpenguins library](https://github.com/allisonhorst/palmerpenguins) in R by Allison Horst.
+""")
+
+st.sidebar.header('User Input Parameters')
+
+st.sidebar.markdown("""
+[Example CSV input file](https://raw.githubusercontent.com/dataprofessor/data/master/penguins_example.csv)
+""")
+
+
+# Function for user input
+def user_input_features():
+        island = st.sidebar.selectbox('Island',('Biscoe','Dream','Torgersen'))
+        sex = st.sidebar.selectbox('Sex',('male','female'))
+        bill_length_mm = st.sidebar.slider('Bill length (mm)', 32.1,59.6,43.9)
+        bill_depth_mm = st.sidebar.slider('Bill depth (mm)', 13.1,21.5,17.2)
+        flipper_length_mm = st.sidebar.slider('Flipper length (mm)', 172.0,231.0,201.0)
+        body_mass_g = st.sidebar.slider('Body mass (g)', 2700.0,6300.0,4207.0)
+        data = {'island': island,
+                'bill_length_mm': bill_length_mm,
+                'bill_depth_mm': bill_depth_mm,
+                'flipper_length_mm': flipper_length_mm,
+                'body_mass_g': body_mass_g,
+                'sex': sex}
+        features = pd.DataFrame(data, index=[0])
+        return features
+
+# Collects user input features into dataframe
+uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type=["csv"])
+if uploaded_file is not None:
+    input_df = pd.read_csv(uploaded_file)
+else:
+    input_df = user_input_features()
+    
+df = input_df
+
+st.subheader('User Input Parameters')
+if uploaded_file is not None:
+    st.write(df)
+else:
+    st.write('Awaiting CSV file to be uploaded. Currently using example input parameters (shown below).')
+    st.write(df)
+
+model = joblib.load(open('Final_RF_Model_14Aug2021.pkl', 'rb'))
+
+# Apply model to make predictions
+pr = model.predict(df)
+
+st.subheader('Prediction')
+penguins_species = np.array(['Adelie','Chinstrap','Gentoo'])
+st.write("The Penguin species is "+penguins_species[pr][0]+".")
+
